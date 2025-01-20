de099587c7ed1e109ea92603f675d426b6a7cb82
Kamaro <kamaroly@gmail.com>
2025-01-20 12:11:29 +0300

    Now it display categories from dbg

--- lib/helpcenter_web/controllers/page_controller.ex
+++ lib/helpcenter_web/controllers/page_controller.ex
@@ -1,9 +1,12 @@
 defmodule HelpcenterWeb.PageController do
+  alias Helpcenter.KnowledgeBase.Category
   use HelpcenterWeb, :controller
 
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+
+    categories = Ash.read!(Category)
+    render(conn, :home, layout: false, categories: categories)
   end
 end
