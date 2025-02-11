29d2bfc3c49356d3ac24c9889c2098f4db399d9e
Kamaro <kamaroly@gmail.com>
2025-02-11 22:24:26 +0300

    Added more filters

---
+++ lib/helpcenter/preparations/now_or_future.ex
@@ -0,0 +1,7 @@
+defmodule Helpcenter.Preparations.NowOrFuture do
+  use Ash.Resource.Preparation
+
+  def prepare(query, _opts, _context) do
+    Ash.Query.filter(query, inserted_at >= ^DateTime.utc_now())
+  end
+end
