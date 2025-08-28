8475d3556efa510370ba7a2fc4453a6435b117ed
Kamaro <kamaroly@gmail.com>
2025-08-28 21:18:40 +0300

    Added tests

--- lib/helpcenter/extensions/ash_parental/ash_parental_test.exs
+++ lib/helpcenter/extensions/ash_parental/ash_parental_test.exs
@@ -1,6 +1,29 @@
-defmodule Helpcenter.Extensions.AshParental do
+defmodule Helpcenter.Extensions.AshParentalTest do
+  use ExUnit.Case
+
+  defmodule Domain do
+    use Ash.Domain
+
+    resources do
+      resource Comment
+    end
+  end
+
   defmodule Comment do
-    use Ash.Resource, extensions: [Helpcenter.Extensions.AshParental]
+    use Ash.Resource,
+      domain: Domain,
+      data_layer: Ash.DataLayer.Ets,
+      extensions: [Helpcenter.Extensions.AshParental]
+
+    ets do
+      table :comments
+
+      private? true
+    end
+
+    attributes do
+      uuid_primary_key :id
+    end
   end
 
   describe "AshParental" do
